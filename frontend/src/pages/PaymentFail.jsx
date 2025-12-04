0d56f049fc0fc13206cba1637af5d8b429a1b278
Hasan <hasannm82@gmail.com>
2025-12-04 20:11:00 +0600

    Adding PaymentFail Page with UI

--- frontend/src/pages/PaymentFail.jsx
+++ frontend/src/pages/PaymentFail.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 const PaymentFail = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-4 py-10 bg-red-50">
+     <h1 className="text-4xl font-bold text-red-700 mb-6">❌ Payment Failed</h1>
       <p className="text-lg text-red-800 mb-6">
         Unfortunately, your payment was not successful. Please try again.
       </p>
